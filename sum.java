68b14d31562bdb6c7b0cd940eee61f9bd3a16e20
MohamedKamaludeen <mohamedkamaludeen48@gmail.com>
2023-01-21 10:54:02 +0530

    This is for new

---
+++ sum.java
@@ -0,0 +1,11 @@
+class sum
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println("The sum is"+(a+b));
+        //this for checking the different
+        //This is a summation opperation
+    }
+}
